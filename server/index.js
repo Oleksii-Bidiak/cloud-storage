2d4e2de22cec82a343704fea3c274cd8474e99c2
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2023-02-09 12:59:01 +0200

    Basic config app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log('start!')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('serverPort')
+
+const start = async () => {
+	try {
+		await mongoose.connect(config.get('dbUrl'))
+
+		app.listen(PORT, () => {
+			console.log(`Сервер запущено на порту ${PORT}`)
+		})
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+start()

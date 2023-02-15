6243903653f58457a348d5846cbd7fcfda69433b
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2023-02-15 11:54:25 +0200

    Create fileUpload endpoint

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,4 +1,6 @@
 const fileServise = require('../servises/fileServise')
+const config = require('config')
+const fs = require('fs')
 const User = require('../models/User')
 const File = require('../models/File')
 
@@ -34,6 +36,52 @@ class FileController {
 		} catch (e) {
 			console.log(req.user)
 			return res.status(500).json({ message: "Can not get files" })
+		}
+	}
+
+	async fileUpload(req, res) {
+		try {
+			const file = req.files.file
+
+			const parent = await File.findOne({ user: req.user.id, _id: req.body.parent })
+			const user = await User.findOne({ _id: req.user.id })
+
+			if (user.usedSpace + file.size > user.diskSpace) {
+				res.status(400).json({ message: "There no space on the disk" })
+			}
+
+			user.usedSpace += file.size
+
+			let path;
+			if (parent) {
+				path = `${config.get('filePath')}\\${user._id}\\${parent.path}\\${file.name}`
+			} else {
+				path = `${config.get('filePath')}\\${user._id}\\${file.name}`
+			}
+
+			if (fs.existsSync(path)) {
+				res.status(400).json({ message: "File already exist" })
+			}
+
+			file.mv(path)
+
+			const type = file.name.split('.').pop()
+			const dbFile = new File({
+				name: file.name,
+				type,
+				size: file.size,
+				path: file?.parent,
+				parent: file?._id,
+				user: user._id
+			})
+
+			await file.save()
+			await user.save()
+
+			res.json(dbFile)
+		} catch (e) {
+			console.log(req.user)
+			return res.status(500).json({ message: "Upload error" })
 		}
 	}
 }

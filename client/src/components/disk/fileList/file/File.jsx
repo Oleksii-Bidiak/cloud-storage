1c375d6921f438ca8c39de23e1861dcce176a118
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2023-02-16 12:17:00 +0200

    Downloading files from the server

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -4,6 +4,7 @@ import dirLogo from "../../../../assets/img/dir.svg";
 import fileLogo from "../../../../assets/img/file.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { pushToStack, setCurrentDir } from "../../../../reducers/fileReducer";
+import { downloadFile } from "../../../../actions/file";
 
 export const File = ({ file }) => {
   const dispatch = useDispatch();
@@ -15,6 +16,11 @@ export const File = ({ file }) => {
       dispatch(setCurrentDir(file._id));
     }
   };
+
+  const downloadClickHandler = (e) => {
+    e.stopPropagation();
+    downloadFile(file);
+  };
   return (
     <div className="file" onClick={() => openDirHandler(file)}>
       <img
@@ -25,6 +31,10 @@ export const File = ({ file }) => {
       <div className="file__name">{file.name}</div>
       <div className="file__date">{file.date.slice(0, 10)}</div>
       <div className="file__size">{file.size}</div>
+      {file.type !== "dir" && (
+        <button className="file__btn file__download" onClick={downloadClickHandler}>Скачати</button>
+      )}
+      <button className="file__btn file__delete">Видалити</button>
     </div>
   );
 };
--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -79,7 +79,6 @@ class FileController {
 				user: user._id
 			});
 
-
 			await dbFile.save()
 			await user.save()
 
@@ -87,6 +86,24 @@ class FileController {
 		} catch (e) {
 			console.log(req.user)
 			return res.status(500).json({ message: "Upload error" })
+		}
+	}
+
+	async downloadFile(req, res) {
+		try {
+
+			const file = await File.findOne({ _id: req.query.id, user: req.user.id })
+			const path = config.get('filePath') + '\\' + req.user.id + '\\' + file.path
+			console.log(path)
+			if (fs.existsSync(path)) {
+				return res.download(path, file.name)
+			}
+
+			return res.status(400).json({ message: "Download error" })
+
+		} catch (e) {
+			console.log(e)
+			res.status(500).json({ message: "Download error" })
 		}
 	}
 }
